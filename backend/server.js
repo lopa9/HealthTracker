3959aad5b626b6f51fbf3eace343ede910455554
lopa9 <lopamudra.p188@gmail.com>
2024-11-05 04:08:03 +0000

    backend updated

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const healthRoutes = require('./routes/healthRoutes'); // Import room routes
+const healthRoutes = require('./routes/healthRoutes'); 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
