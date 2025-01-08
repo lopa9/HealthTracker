631d6ba28701a3cace3201d0e2f0005cce863c80
lopa9 <lopamudra.p188@gmail.com>
2025-01-08 06:40:51 +0000

    .env

--- config/db.js
+++ config/db.js
@@ -1,15 +1,19 @@
-const mongoose = require ("mongoose");
 
-const connectDB = async() => {
-
-    try{
-
-        await mongoose.connect('mongodb+srv://HealthTracker_admin:HealthTracker_admin@healthtracker.atzc9.mongodb.net/Health-db?retryWrites=true&w=majority&appName=HealthTracker');
-        console.log("connected to mongoDB");
-    } catch(err){
-        console.error("Failed to connect to MongoDB", err);
+const mongoose = require('mongoose');
+require('dotenv').config(); // Ensure dotenv is loaded to access environment variables
+const connectDB = async () => {
+    const uri = process.env.DATABASE;
+    if (!uri) {
+        console.error('DATABASE URI is not defined in the environment variables');
+        process.exit(1); // Exit the process if the URI is undefined
+      }
+    try {
+        await mongoose.connect(uri);
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.error('Failed to connect to MongoDB', err);
         process.exit(1);
-
     }
 };
+
 module.exports = connectDB;
